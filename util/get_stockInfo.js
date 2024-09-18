90d9ca5292e0b0fc0f0103a9a2e8070b2f7895f1
nohvenell2 <nohvenell2@gmail.com>
2024-09-19 03:18:31 +0900

    주석수정

--- util/get_stockInfo.js
+++ util/get_stockInfo.js
@@ -1,5 +1,22 @@
 import connectDB from './connectDB.js';
 import isCode from './isCode.js'
+
+/**
+ * 주식 기본 정보
+ * @param {string} code Stock Code
+ * @returns {StockData}
+ * 
+ */
+const get_stockInfo = async (code)=>{
+    if (!isCode(code)) throw new Error('invalid STOCKCODE');
+    const query = 'SELECT * FROM stock_info WHERE stock_code = ?';
+    const conn = await connectDB();
+    const [[result]] = await conn.query(query,[code])
+    conn.end()
+    return result
+}
+export default get_stockInfo
+
 /**
  * @typedef {Object} StockData
  * @property {string} stock_code - 주식 코드
@@ -7,7 +24,7 @@ import isCode from './isCode.js'
  * @property {number} current_price - 현재 가격
  * @property {number} previous_close_price - 전일 종가
  * @property {number} price_change - 가격 변동 (현재 가격 - 전일 가격)
- * @property {number} price_change_sign - 가격 변동 방향 (1: 상승, 2: 하락, 0: 동일)
+ * @property {number} price_change_sign - 가격 변동 방향 (1: 상한, 2: 상승, 3: 보합, 4: 하한, 5: 하락)
  * @property {number} price_change_rate - 가격 변동 비율 (퍼센트)
  * @property {number} accumulated_volume - 누적 거래량
  * @property {number} accumulated_tr_pbmn - 누적 거래대금
@@ -33,19 +50,3 @@ import isCode from './isCode.js'
  * @property {number} pbr - 주가순자산비율
  * @property {number} loan_remain_rate - 대출 잔고율
  */
-
-/**
- * 주식 기본 정보
- * @param {string} code Stock Code
- * @returns {StockData}
- * 
- */
-const get_stockInfo = async (code)=>{
-    if (!isCode(code)) throw new Error('invalid STOCKCODE');
-    const query = 'SELECT * FROM stock_info WHERE stock_code = ?';
-    const conn = await connectDB();
-    const [[result]] = await conn.query(query,[code])
-    conn.end()
-    return result
-}
-export default get_stockInfo
--- util/get_stockPrice.js
+++ util/get_stockPrice.js
@@ -3,23 +3,6 @@ import isCode from './isCode.js';
 import connectDB from './connectDB.js';
 const PERIOD = {'D':'daily','W':'weekly','M':'monthly'};
 
-/**
- * @typedef {Object} StockHistoryData
- * @property {string} stock_code - 주식 코드
- * @property {string} trading_date - 거래일 (YYYY-MM-DD)
- * @property {number} close_price - 종가
- * @property {number} open_price - 시가
- * @property {number} high_price - 최고가
- * @property {number} low_price - 최저가
- * @property {number} accumulated_volume - 누적 거래량
- * @property {number} accumulated_tr_pbmn - 누적 거래대금
- * @property {string} flng_cls_code - 종목 구분 코드
- * @property {number} partition_rate - 분할 비율
- * @property {string} mod_yn - 수정 여부 (Y/N)
- * @property {number} price_change_sign - 가격 변동 방향
- * @property {number} price_change - 가격 변동 (현재 가격 - 전일 가격)
- * @property {string} revl_issu_reas - 공시 사유
- */
 
 /**
  * 
@@ -37,3 +20,22 @@ const get_stockPrice = async(code,period)=>{
     return result
 }
 export default get_stockPrice
+
+
+/**
+ * @typedef {Object} StockHistoryData
+ * @property {string} stock_code - 주식 코드
+ * @property {string} trading_date - 거래일 (YYYY-MM-DD)
+ * @property {number} close_price - 종가
+ * @property {number} open_price - 시가
+ * @property {number} high_price - 최고가
+ * @property {number} low_price - 최저가
+ * @property {number} accumulated_volume - 누적 거래량
+ * @property {number} accumulated_tr_pbmn - 누적 거래대금
+ * @property {string} flng_cls_code - 종목 구분 코드
+ * @property {number} partition_rate - 분할 비율
+ * @property {string} mod_yn - 분할변경여부 (Y,N)
+ * @property {number} price_change_sign - 가격 변동 방향 (1: 상한, 2: 상승, 3: 보합, 4: 하한, 5: 하락)
+ * @property {number} price_change - 가격 변동 (현재 가격 - 전일 가격)
+ * @property {string} revl_issu_reas - 공시 사유
+ */
