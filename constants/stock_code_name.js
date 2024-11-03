408a64953844ba987606bca67fe16a71dbe20b34
nohvenell2 <nohvenell2@gmail.com>
2024-11-03 17:44:35 +0900

    파일정리

--- constants/stock_code_name.js
+++
@@ -1,3 +0,0 @@
-import stock_code_name from '../constants/stock_code_name.json' assert {type:'json'} 
-import stock_code_codes from '../constants/stock_code_codes.json' assert {type:'json'} 
-export {stock_code_codes,stock_code_name}
